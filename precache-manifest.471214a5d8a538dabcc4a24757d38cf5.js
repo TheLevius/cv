0c79277bf9289681b8bc7ca432abf80714278c62
TheLevius <reflevius@gmail.com>
2021-07-07 09:29:41 +0300

    Updates

---
+++ precache-manifest.471214a5d8a538dabcc4a24757d38cf5.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f166aa67d6e767d10716a901433706d",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "14030f9d4e81ea924ae2",
+    "url": "/cv/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "ad9d94a29f767e3f0748",
+    "url": "/cv/static/css/main.bf5f3ba2.chunk.css"
+  },
+  {
+    "revision": "14030f9d4e81ea924ae2",
+    "url": "/cv/static/js/2.5e38e7df.chunk.js"
+  },
+  {
+    "revision": "03f72c3dd3b218a1a429dd1e8e6c1e9f",
+    "url": "/cv/static/js/2.5e38e7df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad9d94a29f767e3f0748",
+    "url": "/cv/static/js/main.ca6e4249.chunk.js"
+  },
+  {
+    "revision": "9e0e921495960402e4a4",
+    "url": "/cv/static/js/runtime-main.c17789b0.js"
+  },
+  {
+    "revision": "4bfa22c31b712bf94869db11a0f68ad2",
+    "url": "/cv/static/media/Jost-Black.4bfa22c3.ttf"
+  },
+  {
+    "revision": "4044db1344a9542546416d5e97876a09",
+    "url": "/cv/static/media/Jost-BlackItalic.4044db13.ttf"
+  },
+  {
+    "revision": "90fc0121ac53dbe0b32eada18636660b",
+    "url": "/cv/static/media/Jost-Bold.90fc0121.ttf"
+  },
+  {
+    "revision": "8d34e9fd494dc090057f2b38c0f537ee",
+    "url": "/cv/static/media/Jost-BoldItalic.8d34e9fd.ttf"
+  },
+  {
+    "revision": "620b052a42968e4e7e40c82b64168b8f",
+    "url": "/cv/static/media/Jost-ExtraBold.620b052a.ttf"
+  },
+  {
+    "revision": "981789381ceca1af0d035bb1781dd254",
+    "url": "/cv/static/media/Jost-ExtraBoldItalic.98178938.ttf"
+  },
+  {
+    "revision": "80b98f22bd6831cd2d803ffeec021ad8",
+    "url": "/cv/static/media/Jost-ExtraLight.80b98f22.ttf"
+  },
+  {
+    "revision": "fe54badb871a3f16e367927bee6a2f0a",
+    "url": "/cv/static/media/Jost-ExtraLightItalic.fe54badb.ttf"
+  },
+  {
+    "revision": "5be78235cb8343126b817af2130f3aed",
+    "url": "/cv/static/media/Jost-Italic.5be78235.ttf"
+  },
+  {
+    "revision": "7f98a7858a75c5aebdc5281eff4e6b5a",
+    "url": "/cv/static/media/Jost-Light.7f98a785.ttf"
+  },
+  {
+    "revision": "e01067ed8787ca7dc793e0e1aa2532bc",
+    "url": "/cv/static/media/Jost-LightItalic.e01067ed.ttf"
+  },
+  {
+    "revision": "55e94bca0c5df2836080235f138a0308",
+    "url": "/cv/static/media/Jost-Medium.55e94bca.ttf"
+  },
+  {
+    "revision": "4e8825ca7111b81ba659e9093f0f88f9",
+    "url": "/cv/static/media/Jost-MediumItalic.4e8825ca.ttf"
+  },
+  {
+    "revision": "31693d0bf15b75c9d0baf033edfeb00f",
+    "url": "/cv/static/media/Jost-Regular.31693d0b.ttf"
+  },
+  {
+    "revision": "d5a291dd16dbe334f56a12ad53b6a058",
+    "url": "/cv/static/media/Jost-SemiBold.d5a291dd.ttf"
+  },
+  {
+    "revision": "0e055181230b32d54b456732b15caece",
+    "url": "/cv/static/media/Jost-SemiBoldItalic.0e055181.ttf"
+  },
+  {
+    "revision": "f1a0dadc29c2b1e21b822ce294a64a3d",
+    "url": "/cv/static/media/Jost-Thin.f1a0dadc.ttf"
+  },
+  {
+    "revision": "7b25755b3660b3d119b9ce51e6090233",
+    "url": "/cv/static/media/Jost-ThinItalic.7b25755b.ttf"
+  },
+  {
+    "revision": "3aba47ab577eb14101eb03ac977e6523",
+    "url": "/cv/static/media/Rubik-Black.3aba47ab.ttf"
+  },
+  {
+    "revision": "8cf69a68225e1e451dce1b020f6b5338",
+    "url": "/cv/static/media/Rubik-BlackItalic.8cf69a68.ttf"
+  },
+  {
+    "revision": "071fcca9ce48f336894f072bae4c2440",
+    "url": "/cv/static/media/Rubik-Bold.071fcca9.ttf"
+  },
+  {
+    "revision": "8aa7fa4bac8320541a6a01b558c53879",
+    "url": "/cv/static/media/Rubik-BoldItalic.8aa7fa4b.ttf"
+  },
+  {
+    "revision": "4b77292b266f2231d56f432424a86df2",
+    "url": "/cv/static/media/Rubik-ExtraBold.4b77292b.ttf"
+  },
+  {
+    "revision": "fa59927d716d7c9ca34785107a9f83d5",
+    "url": "/cv/static/media/Rubik-ExtraBoldItalic.fa59927d.ttf"
+  },
+  {
+    "revision": "25387d7be6e19cdf09b5e8b627ddeaf5",
+    "url": "/cv/static/media/Rubik-Italic.25387d7b.ttf"
+  },
+  {
+    "revision": "a742834a58e74553d141450a7dddf5c0",
+    "url": "/cv/static/media/Rubik-Light.a742834a.ttf"
+  },
+  {
+    "revision": "de021b8d7886b2a0e8c5783d95fb55d5",
+    "url": "/cv/static/media/Rubik-LightItalic.de021b8d.ttf"
+  },
+  {
+    "revision": "11f22f212ab3e9b8e241cbd69d3c43e7",
+    "url": "/cv/static/media/Rubik-Medium.11f22f21.ttf"
+  },
+  {
+    "revision": "56ed0c74858b75d48c461069715efcd5",
+    "url": "/cv/static/media/Rubik-MediumItalic.56ed0c74.ttf"
+  },
+  {
+    "revision": "cd619a4f068dc66cc6e58fe0a91a8a34",
+    "url": "/cv/static/media/Rubik-Regular.cd619a4f.ttf"
+  },
+  {
+    "revision": "75852e6bdc48c05b3c39f4b3b94d2a9c",
+    "url": "/cv/static/media/Rubik-SemiBold.75852e6b.ttf"
+  },
+  {
+    "revision": "54dcba8bf2c662865ee2196d3b66cbfa",
+    "url": "/cv/static/media/Rubik-SemiBoldItalic.54dcba8b.ttf"
+  },
+  {
+    "revision": "544cf996c8d68e70b0670d29ff1318b2",
+    "url": "/cv/static/media/avatarCv2.544cf996.jpg"
+  },
+  {
+    "revision": "33ca79cdc7d21b17184d7a9cb2304a4d",
+    "url": "/cv/static/media/social-network.33ca79cd.svg"
+  },
+  {
+    "revision": "83c8e119149ac53745e4d10d894a1adb",
+    "url": "/cv/static/media/todo-list.83c8e119.svg"
+  }
+]);
